ddd05d7701b610e5412d0dce19eb8c07b33e40d9
emrantiash <tiash03@yahoo.com>
2026-01-18 20:43:16 +0600

    requiremts added

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # chatbot
+
+1. git clone
+
+2. python3.11 -m venv mchatenv 
+
+3. source mchatenv/bin/activate
+
+4. pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="medical chatbot",
+    version="0.0.1",
+    author="Emran Mahmud",
+    author_email="tiashiit904@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
